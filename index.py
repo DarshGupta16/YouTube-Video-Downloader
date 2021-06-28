bf10ae8c781dfd907441b5978fa7d6101c14f6cc
Darsh Gupta <darsh.gupta.1602@gmail.com>
2021-06-28 11:44:34 +0530

    Add video download feature

--- index.py
+++ index.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
+from pytube import YouTube
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/<videoID>')
+def downloadVideo(videoID):
+    video = YouTube('https://www.youtube.com/watch?v=' + videoID)
+    return send_file(video.streams.get_highest_resolution().download(), as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
